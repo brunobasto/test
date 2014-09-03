0a84956005397292ab24cfb862dd4ae75ab410dd
Bruno Basto <bruno.basto@liferay.com>
2014-09-02 21:48:06 -0300

    Adding travis config

---
+++ src/error-listener.js
@@ -0,0 +1,35 @@
+'use strict';
+
+define(function(augment, Base) {
+    var ErrorListener = augment(Base, function(uber) {
+      this.constructor = function() {
+        this.defaults = {
+          handler: function() {}
+        };
+
+        uber.constructor.apply(this, arguments);
+
+        var instance = this;
+
+        window.addEventListener('error', function(event) {
+          var configs = instance.configs,
+              details = instance.details(event);
+
+          configs.handler.apply(instance, [details]);
+        });
+      };
+
+      this.details = function() {
+        return {
+          colno: event.colno,
+          filename: event.filename,
+          lineno: event.lineno,
+          message: event.message,
+          url: location.href
+        };
+      };
+    });
+
+    return ErrorListener;
+  }
+);
